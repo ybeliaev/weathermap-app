c8d361aa4c5774c5cee15c2643366d6604e680ac
Yurij Beliaev <ybeliaev@gmail.com>
2022-01-03 17:43:38 +0200

    refactoring input compon

--- src/Input/index.js
+++ src/Input/index.js
@@ -2,7 +2,22 @@ import React, { useState, useRef } from 'react';
 
 import '../App.css';
 
-const InputTag = ({setCitiesList }) => {
+const InputTag = ({handleOnChange, inputValue, inputRef}) => {   
+    
+    return (
+      <input
+        className="Input"
+        onChange={handleOnChange}
+        value={inputValue}
+        ref={inputRef}
+      />
+    );};
+
+const Button = ({handleOnClick}) => {
+    return <button className="Button" onClick={handleOnClick}>+</button>
+}
+
+export const Input = ({setCitiesList }) => {
     const [inputValue, setInputValue] = useState('')
     const inputRef = useRef(null);
 
@@ -10,29 +25,16 @@ const InputTag = ({setCitiesList }) => {
       setInputValue(event.target.value);
     }
     const handleOnClick = () => {
-        if(inputValue.length){
-            setCitiesList((currentArray) => [...currentArray, inputValue]);
-            setInputValue('');
-            inputRef.current.focus();
-        }
+      if (inputValue.length) {
+        setCitiesList((currentArray) => [...currentArray, inputValue]);
+        setInputValue("");
+        inputRef.current.focus();
+      }
     }
-    
-    return (
-    
-        <input className="Input" onChange={handleOnChange} value={inputValue} ref={inputRef} />
-        
-    
-)};
-
-const Button = () => {
-    return <button className="Button" onClick={handleOnClick}>+</button>
-}
-
-export const Input = () => {
     return (
       <div className="InputWrap">
-        <InputTag />
-        <Button />
+        <InputTag handleOnChange={handleOnChange} inputRef={inputRef} inputValue={inputValue}/>
+        <Button handleOnClick={handleOnClick}/>
       </div>
     );
 }
