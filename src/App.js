2c8dfde195578373f5e7496d2eaaf938e318058b
Yurij Beliaev <ybeliaev@gmail.com>
2022-01-03 17:32:40 +0200

    add state for cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import { Card } from './Card';
 import { Input } from './Input';
 
 function App() {
-  return (
-    <div className="Main">
-      <Input />
-      <div className="CardList">
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </div>
-  );
+  const [citiesList, setCitiesList] = useState([]);
+   return (
+     <div className="Main">
+      <Input setCitiesList={setCitiesList} />
+       <div className="CardList">
+         {citiesList.map((city) => (
+           <Card key={city} city={city} />
+         ))}
+       </div>
+     </div>
+   );
 }
 
 export default App;
--- src/Input/index.js
+++ src/Input/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 import '../App.css';
 
-export const Input = () => (
-    <div className="InputWrap">
-        <input className="Input" />
-        <button className="Button">+</button>
-    </div>
-);
+const InputTag = ({setCitiesList }) => {
+    const [inputValue, setInputValue] = useState('')
+    const inputRef = useRef(null);
+
+    const handleOnChange = (event) => {
+      setInputValue(event.target.value);
+    }
+    const handleOnClick = () => {
+        if(inputValue.length){
+            setCitiesList((currentArray) => [...currentArray, inputValue]);
+            setInputValue('');
+            inputRef.current.focus();
+        }
+    }
+    
+    return (
+    
+        <input className="Input" onChange={handleOnChange} value={inputValue} ref={inputRef} />
+        
+    
+)};
+
+const Button = () => {
+    return <button className="Button" onClick={handleOnClick}>+</button>
+}
+
+export const Input = () => {
+    return (
+      <div className="InputWrap">
+        <InputTag />
+        <Button />
+      </div>
+    );
+}
