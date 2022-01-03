6491381a935d130e888f8e6ea440fa904c9c7432
Yurij Beliaev <ybeliaev@gmail.com>
2022-01-03 18:54:22 +0200

    add hook useCityesList

---
+++ src/hooks/useCitiesList.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+// hook for get and set array of the city list from\to localStorage
+export const useCitiesList = () => {
+  const [citiesList, setCitiesList] = useState(
+    JSON.parse(localStorage("citiesList")) || []
+  );
+  useEffect(() => {
+    localStorage.setItem("citiesList", JSON.stringify(citiesList));
+  }, [citiesList]);
+  return [citiesList, setCitiesList];
+}
