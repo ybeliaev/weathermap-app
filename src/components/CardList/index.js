3d3f8e8b1add4b190a13d51955401cfcb399f0e1
Yurij Beliaev <ybeliaev@gmail.com>
2022-02-04 20:23:50 +0200

    fix err in useState CardList + add handler

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -4,16 +4,25 @@ import {GlobalContext} from "../App";
 
 export const CardList = () => {
     const {state: {citiesList}} = useContext(GlobalContext);
-    const {orderBy, setOrderBy} = useState('asc')
+    const [orderBy, setOrderBy] = useState('asc')
+
+    const handleOnChangeSelect = (e) => {
+        setOrderBy(e.target.value)
+    }
+
+    let sortedCitiesList = citiesList.sort()
+    if(orderBy === 'desc'){
+        sortedCitiesList.reverse()
+    }
     return (
         <>
-            <select className='Select' value={orderBy}>
+            <select className='Select' value={orderBy} onChange={handleOnChangeSelect}>
+                <option value='asc' selected>By name asc</option>
                 <option value='desc'>By name desc</option>
-                <option value='asc'>By name asc</option>
             </select>
             <div className="CardList">
                 {
-                    citiesList.map(city => <Card key={city} city={city}/>)
+                    sortedCitiesList.map(city => <Card key={city} city={city}/>)
                 }
             </div>
         </>
