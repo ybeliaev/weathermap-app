48168a94e081e6ee9a4559d331bb3e137f6fabf8
Yurij Beliaev <ybeliaev@gmail.com>
2022-02-04 19:49:00 +0200

    add select to CardList

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,15 +1,21 @@
 import {Card} from '../Card'
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {GlobalContext} from "../App";
 
 export const CardList = () => {
     const {state: {citiesList}} = useContext(GlobalContext);
+    const {orderBy, setOrderBy} = useState('asc')
     return (
-        <div className="CardList">
-            {
-                citiesList.map(city => <Card key={city} city={city}/>)
-            }
-
-        </div>
+        <>
+            <select className='Select' value={orderBy}>
+                <option value='desc'>By name desc</option>
+                <option value='asc'>By name asc</option>
+            </select>
+            <div className="CardList">
+                {
+                    citiesList.map(city => <Card key={city} city={city}/>)
+                }
+            </div>
+        </>
     )
 }
